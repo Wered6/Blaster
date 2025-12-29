05b1c9f1d62162aa64dcce43554c4b2de10cfdef
Wered6 <arek0006@gmail.com>
2025-12-29 10:28:12 +0100

    41. Seamless Travel and Lobby

---
+++ Source/Blaster/GameModes/BlasterLobbyGameMode.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterLobbyGameMode.h"
+#include "GameFramework/GameStateBase.h"
+
+void ABlasterLobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	const int32 PlayerCount{GameState->PlayerArray.Num()};
+	if (PlayerCount == 2)
+	{
+		UWorld* World{GetWorld()};
+		if (World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Blaster/Maps/Lvl_Blaster?listen"));
+		}
+	}
+}
