fe4701febf3607f3036856500c02dc35c916065c
Wered6 <arek0006@gmail.com>
2026-01-28 11:39:40 +0100

    124

--- Source/Blaster/Player/BlasterPlayerController.cpp
+++ Source/Blaster/Player/BlasterPlayerController.cpp
@@ -8,6 +8,16 @@
 #include "Blaster/HUD/BlasterHUD.h"
 
 
+void ABlasterPlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+
+	if (IsLocalController())
+	{
+		Server_RequestServerTime(GetWorld()->GetTimeSeconds());
+	}
+}
+
 void ABlasterPlayerController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
@@ -39,14 +49,6 @@ void ABlasterPlayerController::OnRep_Pawn()
 	SetHUDHealth(BlasterCharacter->GetHealth(), BlasterCharacter->GetMaxHealth());
 }
 
-void ABlasterPlayerController::ClientSetHUD_Implementation(TSubclassOf<AHUD> NewHUDClass)
-{
-	Super::ClientSetHUD_Implementation(NewHUDClass);
-
-	BlasterHUD = GetHUD<ABlasterHUD>();
-	BlasterHUD->OnCharacterOverlayInitializedDelegate.AddUObject(this, &ABlasterPlayerController::OnCharacterOverlayInitialized);
-}
-
 void ABlasterPlayerController::InitPlayerState()
 {
 	// Happens only on server
@@ -61,20 +63,48 @@ void ABlasterPlayerController::Tick(float DeltaSeconds)
 	Super::Tick(DeltaSeconds);
 
 	SetHUDTime();
+
+	CheckTimeSync(DeltaSeconds);
 }
 
-void ABlasterPlayerController::OnCharacterOverlayInitialized()
+float ABlasterPlayerController::GetServerTime()
 {
-	BlasterCharacterOverlay = BlasterHUD->GetCharacterOverlay();
-
-	bCharacterOverlayValid = true;
-
 	if (HasAuthority())
 	{
-		const ABlasterCharacter* BlasterCharacter{GetPawn<ABlasterCharacter>()};
-		// Happens for local authoritative controller at spawn
-		SetHUDHealth(BlasterCharacter->GetHealth(), BlasterCharacter->GetMaxHealth());
+		return GetWorld()->GetTimeSeconds();
 	}
+	return GetWorld()->GetTimeSeconds() + ClientServerDelta;
+}
+
+void ABlasterPlayerController::CheckTimeSync(const float DeltaSeconds)
+{
+	TimeSyncRunningTime += DeltaSeconds;
+	if (IsLocalController() && TimeSyncRunningTime >= TimeSyncFrequency)
+	{
+		Server_RequestServerTime(GetWorld()->GetTimeSeconds());
+		TimeSyncRunningTime = 0.f;
+	}
+}
+
+void ABlasterPlayerController::Server_RequestServerTime_Implementation(const float TimeOfClientRequest)
+{
+	const float ServerTimeOfReceipt = GetWorld()->GetTimeSeconds();
+	Client_ReportServerTime(TimeOfClientRequest, ServerTimeOfReceipt);
+}
+
+void ABlasterPlayerController::Client_ReportServerTime_Implementation(const float TimeOfClientRequest, const float TimeServerReceivedClientRequest)
+{
+	const float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfClientRequest;
+	const float CurrentServerTime = TimeServerReceivedClientRequest + RoundTripTime / 2.f;
+	ClientServerDelta = CurrentServerTime - GetWorld()->GetTimeSeconds();
+}
+
+void ABlasterPlayerController::ClientSetHUD_Implementation(TSubclassOf<AHUD> NewHUDClass)
+{
+	Super::ClientSetHUD_Implementation(NewHUDClass);
+
+	BlasterHUD = GetHUD<ABlasterHUD>();
+	BlasterHUD->OnCharacterOverlayInitializedDelegate.AddUObject(this, &ABlasterPlayerController::OnCharacterOverlayInitialized);
 }
 
 void ABlasterPlayerController::SetHUDHealth(const float Health, const float MaxHealth) const
@@ -144,6 +174,20 @@ void ABlasterPlayerController::SetHUDCarriedAmmo(const int32 AmmoAmount) const
 	BlasterCharacterOverlayRaw->SetCarriedAmmoAmountText(AmmoAmount);
 }
 
+void ABlasterPlayerController::OnCharacterOverlayInitialized()
+{
+	BlasterCharacterOverlay = BlasterHUD->GetCharacterOverlay();
+
+	bCharacterOverlayValid = true;
+
+	if (HasAuthority())
+	{
+		const ABlasterCharacter* BlasterCharacter{GetPawn<ABlasterCharacter>()};
+		// Happens for local authoritative controller at spawn
+		SetHUDHealth(BlasterCharacter->GetHealth(), BlasterCharacter->GetMaxHealth());
+	}
+}
+
 void ABlasterPlayerController::SetHUDMatchCountdown(const float CountdownTime) const
 {
 	const UBlasterCharacterOverlay* BlasterCharacterOverlayRaw{BlasterCharacterOverlay.Get()};
@@ -162,7 +206,7 @@ void ABlasterPlayerController::SetHUDTime()
 		return;
 	}
 
-	const float RemainingSeconds = MatchTime - GetWorld()->GetTimeSeconds();
+	const float RemainingSeconds = MatchTime - GetServerTime();
 	const uint32 SecondsLeft = FMath::CeilToInt(RemainingSeconds);
 	if (SecondsLeft != Countdown)
 	{
--- Source/Blaster/Player/BlasterPlayerController.h
+++ Source/Blaster/Player/BlasterPlayerController.h
@@ -18,18 +18,55 @@ class BLASTER_API ABlasterPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
+	// Sync with server clock as soon as possible
+	virtual void ReceivedPlayer() override;
+	
 	virtual void OnPossess(APawn* InPawn) override;
 	virtual void OnRep_Pawn() override;
-
-	virtual void ClientSetHUD_Implementation(TSubclassOf<AHUD> NewHUDClass) override;
 	
 	virtual void InitPlayerState() override;
-	
-	virtual void Tick(float DeltaSeconds) override;
-	
-public:	
-	void OnCharacterOverlayInitialized();
 
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	TWeakObjectPtr<ABlasterHUD> BlasterHUD;
+	TWeakObjectPtr<UBlasterCharacterOverlay> BlasterCharacterOverlay;
+
+#pragma region Match Time
+
+public:
+	// Synced with server world clock
+	virtual float GetServerTime();
+	
+private:
+	// Requests the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void Server_RequestServerTime(const float TimeOfClientRequest);
+	
+	// Reports the current server time to the client in response to Server_RequestServerTime
+	UFUNCTION(Client, Reliable)
+	void Client_ReportServerTime(const float TimeOfClientRequest, const float TimeServerReceivedClientRequest);
+	
+	void CheckTimeSync(float DeltaSeconds);
+	
+	// Difference between client and server time
+	float ClientServerDelta{0.f};
+	
+	UPROPERTY(EditDefaultsOnly, Category="Blaster|Match Time")
+	float TimeSyncFrequency{5.f};
+	
+	float TimeSyncRunningTime{0.f};
+	
+	float MatchTime{120.f};
+	uint32 Countdown{0};
+
+#pragma endregion
+
+#pragma region HUD
+
+public:
+	virtual void ClientSetHUD_Implementation(TSubclassOf<AHUD> NewHUDClass) override;
+	
 	void SetHUDHealth(const float Health, const float MaxHealth) const;
 
 	void SetHUDScore(const float Score) const;
@@ -41,16 +78,14 @@ public:
 	void SetHUDWeaponAmmo(const int32 AmmoAmount) const;
 	void SetHUDCarriedAmmo(const int32 AmmoAmount) const;
 
+private:
+	void OnCharacterOverlayInitialized();
+	
 	void SetHUDMatchCountdown(float CountdownTime) const;
 
-private:
 	void SetHUDTime();
-	
+
 	bool bCharacterOverlayValid{false};
 
-	float MatchTime{120.f};
-	uint32 Countdown{};
-
-	TWeakObjectPtr<ABlasterHUD> BlasterHUD;
-	TWeakObjectPtr<UBlasterCharacterOverlay> BlasterCharacterOverlay;
+#pragma endregion
 };
